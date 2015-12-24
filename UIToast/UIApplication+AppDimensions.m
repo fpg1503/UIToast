815ae973ff992eb3b415c6014a5b527d28e63f44
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2015-12-24 10:18:54 -0800

    Fix landscape on iOS 8+ (#4)

--- UIToast/UIApplication+AppDimensions.m
+++ UIToast/UIApplication+AppDimensions.m
@@ -18,14 +18,17 @@
 }
 
 + (CGSize)sizeInOrientation:(UIInterfaceOrientation)orientation {
+    
     CGSize size = [UIScreen mainScreen].bounds.size;
-    UIApplication *application = [UIApplication sharedApplication];
-    if (UIInterfaceOrientationIsLandscape(orientation)) {
-        size = CGSizeMake(size.height, size.width);
-    }
-    if (application.statusBarHidden == NO) {
-        size.height -= MIN(application.statusBarFrame.size.width,
-                           application.statusBarFrame.size.height);
+    if (NSFoundationVersionNumber <= NSFoundationVersionNumber_iOS_7_1) {
+        UIApplication *application = [UIApplication sharedApplication];
+        if (UIInterfaceOrientationIsLandscape(orientation)) {
+            size = CGSizeMake(size.height, size.width);
+        }
+        if (application.statusBarHidden == NO) {
+            size.height -= MIN(application.statusBarFrame.size.width,
+                               application.statusBarFrame.size.height);
+        }
     }
     return size;
 }
